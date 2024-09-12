fa910fc07a32659333b14d5011434c6d3b0f77bf
Robert-Stackflow <yutuan.victory@gmail.com>
2024-09-13 00:20:31 +0800

    Fix action bugs

---
+++ tools/ios_tools/thin_payload.sh
@@ -0,0 +1,29 @@
+foreachThin(){
+  for file in $1/*
+  do
+      if test -f $file
+      then
+           mime=$(file --mime-type -b $file)
+           if [ "$mime" == 'application/x-mach-binary' ]  || [ "${file##*.}"x = "dylib"x ]
+           then
+                echo thin $file
+                xcrun -sdk iphoneos lipo "$file" -thin arm64 -output "$file"
+                xcrun -sdk iphoneos bitcode_strip "$file" -r -o  "$file"
+                strip -S -x "$file" -o "$file"
+           fi
+      fi
+      if test -d $file
+      then
+          foreachThin $file
+      fi
+  done
+}
+
+# shellcheck disable=SC1072
+# shellcheck disable=SC1073
+# shellcheck disable=SC1009
+if [ $# eq 0 ]; then
+  echo "no argument"
+else
+  foreachThin $1
+fi
